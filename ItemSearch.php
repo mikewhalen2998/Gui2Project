04d68620c0a2d6965f52695f949312c99a3f3b8f
CGonthier424 <Connor.Gonthier@gmail.com>
2021-04-06 18:32:26 -0400

    Finished ItemSearch and Upload functionality.
    Finished ItemSearch and Upload functionality. Need to work on styling and displaying the information taken from item search

--- ItemSearch.php
+++ ItemSearch.php
@@ -1,5 +1,49 @@
 <?php
+$IName=$_POST["ItemName"];
+$IType=$_POST["itemType"];
 
-Echo"I am lazy and havent done this, gg no re";
+echo"ItemName: ".$IName;
+?> <br><?php
+echo"Item Type: ".$IType;
+?> <br><?php
+?>
+<a href="ItemUpload.html">
+      <input type="submit" value ="Return to Item Seach"/>
+</a>
+<br><br>
+<?php
+
+//Opening Connection to database and testing connection
+$dbConnection = new mysqli('localhost', 'root', '', 'gui2');
+if ($dbConnection->connect_error) {
+  die("Connection failed: " . $dbConnection->connect_error);
+}
+
+$stmt = $dbConnection->prepare("Select * FROM items WHERE itemName like CONCAT( '%',?,'%') AND itemType = ? ");
+if(false ===$stmt){
+  die('prepare() failed: ' . htmlspecialchars($mysqli->error));
+}
+$check = $stmt->bind_param("ss", $IName, $IType);
+if(false ===$check){
+  die('bind_param() failed: ' . htmlspecialchars($stmt->error));
+}
+$check = $stmt->execute();
+if(false ===$check){
+  die('execute() failed: ' . htmlspecialchars($stmt->error));
+}
+$selectedItems = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+ $stmt->close();
+ //var_dump($selectedItems);
+
+?> <br> <?php
+for($i=0; $i<count($selectedItems);$i++){
+  echo"Item Name: ".$selectedItems[$i]["itemName"];
+  echo"   Aisle Number: ".$selectedItems[$i]["aisleNumber"];
+  echo"   Image:  ".$selectedItems[$i]["image"];
+  echo"   Item Type:  ".$selectedItems[$i]["itemType"];
+?> <br><br> <?php
+
+}
+
 
 ?>
--- ItemUpload.php
+++ ItemUpload.php
@@ -1,7 +1,9 @@
 <?php
+//Add styling to files for when users using return button
 // take location input and push user to Selector.HTML
 $IName = $_POST["ItemName"];
 $ANum = $_POST["AisleNum"];
+$IType = $_POST["itemType"];
 
 $uploaddir= 'img/';
 $uploadfile = $uploaddir.basename($_FILES['userfile']['name']);
@@ -10,6 +12,9 @@ echo"ItemName: ".$IName;
 ?> <br><?php
 echo"AisleNum: ".$ANum;
 ?> <br><?php
+echo"Item Type: ".$IType;
+?> <br><?php
+
 
 echo"Uploadfile: ".$uploadfile;
 ?> <br><?php
@@ -26,14 +31,30 @@ echo 'Here is some more debugging info:';
 print_r($_FILES);
 
 print "</pre>";
-
-
+?>
+<a href="ItemUpload.html">
+      <input type="submit" value ="Return to Item Upload"/>
+</a>
+<br><br>
+<?php
 //After here upload to database
-
-
-
-
-
-
-
+//Opening Connection to database and testing connection
+$dbConnection = new mysqli('localhost', 'root', '', 'gui2');
+if ($dbConnection->connect_error) {
+  die("Connection failed: " . $dbConnection->connect_error);
+}
+//Using prepares statments to insert all the item information collected from the html page
+//into our database, printing out errors at each location should there be an error
+$stmt = $dbConnection->prepare("INSERT INTO items (itemName, aisleNumber, image, itemType) VALUES (?, ?, ?, ?)");
+if(false ===$stmt){
+  die('prepare() failed: ' . htmlspecialchars($mysqli->error));
+}
+$check = $stmt->bind_param("ssss", $IName, $ANum, $uploadfile,$IType);
+if(false ===$check){
+  die('bind_param() failed: ' . htmlspecialchars($stmt->error));
+}
+$check = $stmt->execute();
+if(false ===$check){
+  die('execute() failed: ' . htmlspecialchars($stmt->error));
+}
 ?>
