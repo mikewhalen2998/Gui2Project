fc3497bc31c2fd30865cd53c8f48be933bd49b0a
mikewhalen2998 <mike.whalen2998@gmail.com>
2021-04-08 18:21:37 -0400

    Adding styling for things

--- ItemSearch.php
+++ ItemSearch.php
@@ -1,15 +1,44 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<link rel="stylesheet" href="searchphpstyle.css">
+
+<title>GSA Search Page</title>
+</head>
+
+<body>
+
+<!-- Header with clickable link to homepage -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="homepage.html">Grocery Shopping Assistant</a>
+  </nav>
+
+<!-- This div is for printing out what the user entered at the top left. For ex: Pizza and then they select Frozen in the selector -->
+<div class="searchInformation">
 <?php
 $IName=$_POST["ItemName"];
 $IType=$_POST["itemType"];
+
 
 echo"ItemName: ".$IName;
 ?> <br><?php
 echo"Item Type: ".$IType;
 ?> <br><?php
 ?>
-<a href="ItemUpload.html">
-      <input type="submit" value ="Return to Item Seach"/>
+
+
+<!-- This button will return them to the search page if they'd like to search for another item -->
+<a href="ItemSearch.html">
+      <input type="submit" value ="Search for Another Item"/>
 </a>
+</div>  <!-- Closing out of searchInformation div -->
 <br><br>
 <?php
 
@@ -34,16 +63,44 @@ if(false ===$check){
 $selectedItems = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
  $stmt->close();
  //var_dump($selectedItems);
-
-?> <br> <?php
-for($i=0; $i<count($selectedItems);$i++){
-  echo"Item Name: ".$selectedItems[$i]["itemName"];
-  echo"   Aisle Number: ".$selectedItems[$i]["aisleNumber"];
-  echo"   Image:  ".$selectedItems[$i]["image"];
-  echo"   Item Type:  ".$selectedItems[$i]["itemType"];
-?> <br><br> <?php
-
-}
-
-
 ?>
+
+<br>
+<!-- This div will pertain to the information about the item that is printed out on the screen. Ex: Item name, Aisle number, photo, Item type -->
+<div class="foodInformation">
+ <?php
+for($i=0; $i<count($selectedItems);$i++){
+  ?>
+  <div class="itemNameStyling">
+  <?php
+  echo"Item Name: ".$selectedItems[$i]["itemName"];
+  ?>
+</div> <!--itemName Styling close -->
+
+  <div class="aisleNumberStyling">
+  <?php
+  echo"   Aisle Number: ".$selectedItems[$i]["aisleNumber"];
+  ?>
+</div> <!-- aisleNumberStyling close -->
+
+    <div class="imageStyling">
+    <img src="<?php echo $selectedItems[$i]["image"]?>" alt="Image not displayed">
+</div> <!-- imageStyling close -->
+
+    <div class="itemTypeStyling">
+    <?php
+  echo"   Item Type:  ".$selectedItems[$i]["itemType"];
+?>
+</div> <!-- itemTypeStyling close div -->
+ <hr> <!-- Line below each set of information per product -->
+ <br><!-- These are just for spacing -->
+ <br><!-- These are just for spacing -->
+<?php
+} // Close of for loop
+?>
+</div> <!-- close of foodInformation div -->
+
+<!-- Scripts and close out tags for body and html -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
--- ItemUpload.php
+++ ItemUpload.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link rel="stylesheet" href="itemUploadStyle.css">
+<title>GSA Item Upload</title>
+</head>
+
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="homepage.html">Grocery Shopping Assistant</a>
+  </nav>
+
+<div class="iteminfoPrintout">
 <?php
 //Add styling to files for when users using return button
 // take location input and push user to Selector.HTML
@@ -27,11 +47,14 @@ if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
     echo "Possible file upload attack!\n";
 }
 
+/*
 echo 'Here is some more debugging info:';
 print_r($_FILES);
 
 print "</pre>";
+*/
 ?>
+</div><!-- itemInfoPrintout close -->
 <a href="ItemUpload.html">
       <input type="submit" value ="Return to Item Upload"/>
 </a>
@@ -58,3 +81,7 @@ if(false ===$check){
   die('execute() failed: ' . htmlspecialchars($stmt->error));
 }
 ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
